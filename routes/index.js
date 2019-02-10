bea1c32935036c0c02cc509e26b32a3b9143d8d8
Md Shayon <mdshayon0@gmail.com>
2019-02-11 02:59:40 +0600

    Add explanation about slice methods

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,10 @@ router.get('/', function(req, res, next) {
     // REST OF THEM WILL GO THO ANOTHER ROW
     chunkSize = 3;
     for(let i = 0; i<docs.length ; i+= chunkSize){
-      productChunks.push(docs.slice(i, i+chunkSize))
+      // For example, slice(1,4) extracts the second element through the fourth element (elements indexed 1, 2, and 3).
+      // know more about slice methods
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/slice
+      productChunks.push(docs.slice(i, i+chunkSize));
     }
     res.render('shop/index', { title: 'Shoping Cart', products: productChunks });
   });
